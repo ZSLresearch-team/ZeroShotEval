6cf0263dd6b662d4bb198322a6f9e9cdddd37720
Nikita Nikolaev <nikolaev.nick959@gmail.com>
2020-01-31 17:49:56 +0700

    Add template for main train script, updated config

--- config.py
+++ config.py
@@ -1,109 +1,170 @@
-"""Config file for ZSLConstructor
+"""Config file for ZeroShotEval launcher scripts
 
-This module this module contains all config settings for ZSLConstuctor in edicts.
+This module contains config settings for all modules in ZeroShotEval toolkit 
+in the format of easydict dictionaries.
 
 
-Todo:
-    * Add AWA2, SUN, aPY datasets conf
-    * Add GAN conf
-    * Add embeddings conf
-    * etc.
+Already contains the following configs:
 
+Neural networks:
+    - CADA-VAE (incomplete)  \\TODO: Verify CADA-VAE configs
+    - 
+
+Datasets:
+    - CUB (incomplete)  \\TODO: Verify CUB configs
 """
 
+# TODO:
+#     * Add AWA2, SUN datasets conf
+#     * Add GAN conf
+#     * Add embeddings conf
+#     * etc.
 
-from  easydict import EasyDict as edict
 
-# Model config
+from easydict import EasyDict as edict
+
+
+#region GLOBAL DEFAULT CONFIGS
+config = edict()
+
+config.load_raw_modalities = False
+config.modalities = ['img', 'cls_attr']
+config.img_net = 'resnet101'
+config.cls_attr_net = 'word2vec'
+
+config.load_dataset_precomputed_embeddings = True
+config.load_cached_obj_embeddings = False
+config.cache_obj_embeddings = True  # recommended always True
+
+config.model = 'cada_vae'
+config.datasets = ['cub']
+#endregion
+
+
+#region MODEL CONFIGS
 model = edict()
 
-#Общие гиперпараметры для всех моделей
-model.general_hyperparameters = edict()
-# Возможно стоит перетащить эти параметры в default?
+model.general_hyperparameters = edict()  # general hyperparameters for all models
 model.general_hyperparameters.device = "cuda"
 model.general_hyperparameters.num_shots = 0
 model.general_hyperparameters.generalized = True
 model.general_hyperparameters.batch_size = 32
 model.general_hyperparameters.nepoch = 100
-model.general_hyperparameters.fp16_train_mode = False #Для *сверхлюдей* обладающих GPU с тензорными-ядрами
+model.general_hyperparameters.fp16_train_mode = False  # for GPUs with tensor cores
 
 
-model.CADA_VAE = edict()
-model.CADA_VAE.module_name = "models.CADA_VAE_net"
-model.CADA_VAE.class_name = "CADA_VAE"
-model.CADA_VAE.cross_resonstuction = True
-model.CADA_VAE.distance = "Wasserstein"
+#region CADA_VAE CONFIGS
+model.cada_vae = edict()
+model.cada_vae.model_name = "cada_vae"
+# model.CADA_VAE.class_name = "CADA_VAE"
+model.cada_vae.cross_resonstuction = True
+model.cada_vae.distance = "wasserstein"
 
-#Специфичные гипперпараметры для CADA_VAE
-model.CADA_VAE.specific_paraneters = edict()
-model.CADA_VAE.specific_paraneters.lr_gen_model = 0.00015
-model.CADA_VAE.specific_paraneters.loss = 'l1'
-model.CADA_VAE.specific_paraneters.latent_size = 64
-model.CADA_VAE.specific_paraneters.lr_cls = 0.001
-model.CADA_VAE.specific_paraneters.cls_train_epochs = 100# early stopping nepoch стоит изменить
-model.CADA_VAE.specific_paraneters.auxiliary_data_source = 'attributes'#!для общности следуюет переделать эту и
-#связанные части
+model.cada_vae.specific_paraneters = edict()
+model.cada_vae.specific_paraneters.lr_gen_model = 0.00015
+model.cada_vae.specific_paraneters.loss = 'l1'
+model.cada_vae.specific_paraneters.latent_size = 64
+model.cada_vae.specific_paraneters.lr_cls = 0.001
+model.cada_vae.specific_paraneters.cls_train_epochs = 100  # early stopping nepoch стоит изменить
+model.cada_vae.specific_paraneters.auxiliary_data_source = 'attributes'  # для общности следует переделать эту и связанные части
 
-#######################################################################################################################
-# !NB эти парамертры стоит извлекать из генераторов эмбедингов/кэшированных эмбедингов. Их нужно перенести в dataset
-# !куда-то ещё. Стоит развести скрытые слои для декодера/энкодера?
-#######################################################################################################################
-model.CADA_VAE.specific_paraneters.hidden_layers = edict()
-model.CADA_VAE.specific_paraneters.hidden_layers.cnn_features = (1560, 1660)
-model.CADA_VAE.specific_paraneters.hidden_layers.attributes = (1450, 665)
-model.CADA_VAE.specific_paraneters.hidden_layers.sentences = (1450, 665)
-model.CADA_VAE.specific_paraneters.input_features_from_cnn = 2048 #для резнета101
 
-model.CADA_VAE.specific_paraneters.warmup = edict()
+# NOTE: эти парамертры стоит извлекать из генераторов эмбедингов/кэшированных эмбедингов.
+# Их нужно перенести в dataset или куда-то ещё. 
+# 
+# Стоит ли развести скрытые слои для декодера/энкодера?
 
-model.CADA_VAE.specific_paraneters.warmup.beta = edict()
-model.CADA_VAE.specific_paraneters.warmup.beta.factor = 0.25
-model.CADA_VAE.specific_paraneters.warmup.beta.end_epoch = 93
-model.CADA_VAE.specific_paraneters.warmup.beta.start_epoch = 0
 
-model.CADA_VAE.specific_paraneters.warmup.cross_reconstuction = edict()
-model.CADA_VAE.specific_paraneters.warmup.cross_reconstuction.factor = 2.37
-model.CADA_VAE.specific_paraneters.warmup.cross_reconstuction.end_epoch = 75
-model.CADA_VAE.specific_paraneters.warmup.cross_reconstuction.start_epoch = 21
+model.cada_vae.specific_paraneters.hidden_layers = edict()
+model.cada_vae.specific_paraneters.hidden_layers.cnn_features = (1560, 1660)
+model.cada_vae.specific_paraneters.hidden_layers.attributes = (1450, 665)
+model.cada_vae.specific_paraneters.hidden_layers.sentences = (1450, 665)
+model.cada_vae.specific_paraneters.input_features_from_cnn = 2048  # for ResNet101
 
-model.CADA_VAE.specific_paraneters.warmup.distance = edict()
-model.CADA_VAE.specific_paraneters.warmup.distance.factor = 8.13
-model.CADA_VAE.specific_paraneters.warmup.distance.end_epoch = 22
-model.CADA_VAE.specific_paraneters.warmup.distance.start_epoch = 6
+model.cada_vae.specific_paraneters.warmup = edict()
+model.cada_vae.specific_paraneters.warmup.beta = edict()
+model.cada_vae.specific_paraneters.warmup.beta.factor = 0.25
+model.cada_vae.specific_paraneters.warmup.beta.end_epoch = 93
+model.cada_vae.specific_paraneters.warmup.beta.start_epoch = 0
 
-# Dataset settings
+model.cada_vae.specific_paraneters.warmup.cross_reconstuction = edict()
+model.cada_vae.specific_paraneters.warmup.cross_reconstuction.factor = 2.37
+model.cada_vae.specific_paraneters.warmup.cross_reconstuction.end_epoch = 75
+model.cada_vae.specific_paraneters.warmup.cross_reconstuction.start_epoch = 21
+
+model.cada_vae.specific_paraneters.warmup.distance = edict()
+model.cada_vae.specific_paraneters.warmup.distance.factor = 8.13
+model.cada_vae.specific_paraneters.warmup.distance.end_epoch = 22
+model.cada_vae.specific_paraneters.warmup.distance.start_epoch = 6
+#endregion
+
+#region CLSWGAN CONFIGS
+model.clswgan = edict()
+model.clswgan.model_name = 'clswgan'
+# TODO: complete CLSWGAN CONFIGS section
+#endregion
+
+#endregion
+
+
+#region DATASET CONFIGS
 dataset = edict()
 
-dataset.CUB = edict()
+#region CUB DATASET CONFIGS
+dataset.cub = edict()
+dataset.cub.dataset_name = 'cub'
+dataset.cub.path = "data/CUB_200_2011/"
+dataset.cub.precomputed_embeddings_path = 'data/CUB/res101.mat'
 
-dataset.CUB.path = ""
-dataset.CUB.num_classes = 200
-dataset.CUB.num_novel_classes = 50
-dataset.CUB.samples_per_class = (200, 0, 400, 0) # ! Будет меняться от generalized (num_shots == 0)
-                                                 # ! данные значение для GZSL. Стоит изменть
+dataset.cub.num_classes = 200
+dataset.cub.num_novel_classes = 50
+dataset.cub.samples_per_class = (200, 0, 400, 0) # ! Будет меняться от generalized (num_shots == 0). Данные значение для GZSL
+# TODO: Стоит изменть для общности предыдущую строку
 
-dataset.CUB.class_embedding = edict()
+dataset.cub.class_embedding = edict()
 
-dataset.CUB.class_embedding.description_emb = edict()
-dataset.CUB.class_embedding.description_emb.module_name = ""
-dataset.CUB.class_embedding.description_emb.class_name = ""
-dataset.CUB.class_embedding.description_emb.have_pretrained = True
-dataset.CUB.class_embedding.description_emb.path = ""
+dataset.cub.class_embedding.description_emb = edict()
+dataset.cub.class_embedding.description_emb.module_name = ""
+dataset.cub.class_embedding.description_emb.class_name = ""
+dataset.cub.class_embedding.description_emb.have_pretrained = True
+dataset.cub.class_embedding.description_emb.path = ""
 
-dataset.CUB.object_embedding = edict()
+dataset.cub.object_embedding = edict()
 
-dataset.CUB.object_embedding.resnet101 = edict()
+dataset.cub.object_embedding.resnet101 = edict()
 
-dataset.CUB.object_embedding.resnet101 = edict()
-dataset.CUB.object_embedding.resnet101.module_name = ""
-dataset.CUB.object_embedding.resnet101.class_name = ""
-dataset.CUB.object_embedding.resnet101.have_pretrained = True
-dataset.CUB.object_embedding.resnet101.path = ""
+dataset.cub.object_embedding.resnet101.module_name = ""
+dataset.cub.object_embedding.resnet101.class_name = ""
+dataset.cub.object_embedding.resnet101.have_pretrained = True
+dataset.cub.object_embedding.resnet101.path = ""
+#endregion
+
+#region AWA2 DATASET CONFIGS
+dataset.awa2 = edict()
+dataset.awa2.dataset_name = "awa2"
+# TODO: complete AWA2 DATASET CONFIGS section
+#endregion 
+
+#endregion
 
 
+#region DEFAULT CONFIGS
 default = edict()
 
-default.model = "CADA_VAE"
-default.dataset = "CUB"
+default.model = "cada_vae"
+default.dataset = "cub"
 default.class_embedding = "description_emb"
 default.object_embedding = "resnet101"
+#endregion
+
+
+def generate_config(parsed_model, parsed_datasets):
+    for key, value in model[parsed_model].items():
+        config[key] = value
+    for _dataset in parsed_datasets:
+        config[_dataset] = edict()
+        for key, value in dataset[_dataset].items():
+            config[_dataset][key] = value
+    config.model = parsed_model
+    config.datasets = parsed_datasets
+    
