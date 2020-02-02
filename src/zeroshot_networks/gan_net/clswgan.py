018ac31be08c79b7dfb1517d5b06f2a7e0ab6ef6
Krisantis <Krisantis@mail.ru>
2020-02-02 05:46:45 -0800

    gan MVP

--- src/zeroshot_networks/gan_net/clswgan.py
+++ src/zeroshot_networks/gan_net/clswgan.py
@@ -4,6 +4,9 @@ import tensorflow as tf
 import sys
 import random
 
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+os.environ["TF_CPP_MIN_LOG_LEVEL"]='3'
+
 class CLSWGAN:
     def __init__(self, _seenclasses, _epochCount=2000, _critic_iter=5):
         """
@@ -19,12 +22,13 @@ class CLSWGAN:
 
 
 
-    def initDefoltModel(self,batch_size=64,resSize=2048,attSize=1024,nz=312):
+    def initDefoltModel(self,batch_size=64,resSize=2048,attSize=312,nz=312,_beta1=0.5):
         """
             batch_size: input batch size
             resSize: size of visual features
             attSize: size of semantic features
-            nz: size of the latent z vector            
+            nz: size of the latent z vector          
+            _beta1: beta1 for adam. default=0.5
         """
         self.batch_size = batch_size
         self.resSize = resSize
@@ -36,21 +40,21 @@ class CLSWGAN:
         
         with self.g1.as_default():
 
-            inputImgEmbd    = tf.placeholder(tf.float32,[batch_size, resSize],name='input_features')
-            inputTxtEmbd    = tf.placeholder(tf.float32,[batch_size, attSize],name='input_attributes')
-            inputNoiseImg   = tf.placeholder(tf.float32,[batch_size, nz],name='noise')
-            inputLabel      = tf.placeholder(tf.int32,[batch_size],name='input_label')
-            lambda1         = tf.placeholder(tf.int32,name='gradient_penalty_regularizer')
-            cls_weight      = tf.placeholder(tf.int32,name='weight_classification_loss')
-            lr              = tf.placeholder(tf.int32,name='learning_rate_GANs')
-            beta1           = tf.placeholder(tf.int32,name='adamBeta1')
+            self.inputImgEmbd    = tf.placeholder(tf.float32,[self.batch_size, self.resSize],name='input_features')
+            self.inputTxtEmbd    = tf.placeholder(tf.float32,[self.batch_size, self.attSize],name='input_attributes')
+            self.inputNoiseImg   = tf.placeholder(tf.float32,[self.batch_size, self.nz],name='noise')
+            self.inputLabel      = tf.placeholder(tf.int32,[self.batch_size],name='input_label')
+            self.lambda1         = tf.placeholder(tf.float32,shape=[],name='gradient_penalty_regularizer')
+            self.cls_weight      = tf.placeholder(tf.float32,shape=[],name='weight_classification_loss')
+            self.lr              = tf.placeholder(tf.float32,shape=[],name='learning_rate_GANs')
+            self.beta1           = _beta1
            
 
             # model definition
             train = True
             reuse = False
 
-            inputGenEmb = tf.concat([inputNoiseImg, inputTxtEmbd], axis=1)
+            inputGenEmb = tf.concat([self.inputNoiseImg, self.inputTxtEmbd], axis=1)
             
             gen_res = self.defoltGenerator( x=inputGenEmb,
                                             ngh=hiddenLayerGenSize,
@@ -62,13 +66,13 @@ class CLSWGAN:
                                                                 classes=self.seenclasses.shape[0],
                                                                 isTrainable=False,
                                                                 reuse=reuse)
-            targetEmbd = tf.concat([inputImgEmbd,inputTxtEmbd], axis=1)
+            targetEmbd = tf.concat([self.inputImgEmbd,self.inputTxtEmbd], axis=1)
 
             targetDisc = self.defoltDiscriminator(  x=targetEmbd,
                                                     ndh=hiddenLayerDescSize,
                                                     isTrainable=train,
                                                     reuse=reuse)
-            genTargetEmbd = tf.concat([gen_res,inputTxtEmbd], axis=1)
+            genTargetEmbd = tf.concat([gen_res,self.inputTxtEmbd], axis=1)
             genTargetDisc = self.defoltDiscriminator(   x=genTargetEmbd,
                                                         ndh=hiddenLayerDescSize,
                                                         isTrainable=train,
@@ -76,20 +80,20 @@ class CLSWGAN:
             
             #classification loss
             spSfEntr = tf.nn.sparse_softmax_cross_entropy_with_logits(  logits=classificationLogits,
-                                                                        labels=inputLabel)
+                                                                        labels=self.inputLabel)
             classificationLoss = tf.reduce_mean(spSfEntr)
 
             #discriminator loss
             
             genDiscMean = tf.reduce_mean(genTargetDisc)
             targetDiscMean = tf.reduce_mean(targetDisc)
-            discriminatorLoss = tf.reduce_mean(genTargetDisc - targetDisc)
+            self.discriminatorLoss = tf.reduce_mean(genTargetDisc - targetDisc)
             alpha = tf.random_uniform(shape=[batch_size,1], minval=0.,maxval=1.)
 
             #differences = genTargetEnc - targetEnc
             #interpolates = targetEnc + (alpha*differences)
-            interpolates = alpha * inputImgEmbd + ((1 - alpha) * gen_res)
-            interpolate = tf.concat([interpolates, inputTxtEmbd], axis=1)
+            interpolates = alpha * self.inputImgEmbd + ((1 - alpha) * gen_res)
+            interpolate = tf.concat([interpolates, self.inputTxtEmbd], axis=1)
 
             gradients = tf.gradients(   self.defoltDiscriminator(   x=interpolate,
                                                                     ndh=hiddenLayerDescSize,
@@ -100,12 +104,12 @@ class CLSWGAN:
             slopes = tf.sqrt(tf.reduce_sum(tf.square(gradients), reduction_indices=[1]))
             gradientPenalty = tf.reduce_mean((slopes-1.)**2)   
                 
-            gradientPenalty = lambda1*gradientPenalty
-            discriminatorLoss = discriminatorLoss + gradientPenalty
+            gradientPenalty = self.lambda1*gradientPenalty
+            self.discriminatorLoss = self.discriminatorLoss + gradientPenalty
 
             #Wasserstein generator loss
             genLoss = -genDiscMean
-            generatorLoss = genLoss + cls_weight*classificationLoss
+            self.generatorLoss = genLoss + self.cls_weight*classificationLoss
 
             #################### getting parameters to optimize ####################
             discParams = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='discriminator')
@@ -118,24 +122,24 @@ class CLSWGAN:
             for params in generatorParams:
                 print (params.name)
 
-            discOptimizer = tf.train.AdamOptimizer( learning_rate=lr,
-                                                    beta1=beta1,
+            discOptimizer = tf.train.AdamOptimizer( learning_rate=self.lr,
+                                                    beta1=self.beta1,
                                                     beta2=0.999)
 
-            genOptimizer = tf.train.AdamOptimizer(  learning_rate=lr,
-                                                    beta1=beta1,
+            genOptimizer = tf.train.AdamOptimizer(  learning_rate=self.lr,
+                                                    beta1=self.beta1,
                                                     beta2=0.999)
 
-            discGradsVars = discOptimizer.compute_gradients(discriminatorLoss,var_list=discParams)    
-            genGradsVars = genOptimizer.compute_gradients(generatorLoss,var_list=generatorParams)
+            discGradsVars = discOptimizer.compute_gradients(self.discriminatorLoss,var_list=discParams)    
+            genGradsVars = genOptimizer.compute_gradients(self.generatorLoss,var_list=generatorParams)
 
-            discTrain = discOptimizer.apply_gradients(discGradsVars)
-            generatorTrain = genOptimizer.apply_gradients(genGradsVars)
+            self.discTrain = discOptimizer.apply_gradients(discGradsVars)
+            self.generatorTrain = genOptimizer.apply_gradients(genGradsVars)
 
             #################### what all to visualize  ############################
-            tf.summary.scalar("DiscriminatorLoss",discriminatorLoss)
+            tf.summary.scalar("DiscriminatorLoss",self.discriminatorLoss)
             tf.summary.scalar("ClassificationLoss",classificationLoss)
-            tf.summary.scalar("GeneratorLoss",generatorLoss)
+            tf.summary.scalar("GeneratorLoss",self.generatorLoss)
             tf.summary.scalar("GradientPenaltyTerm",gradientPenalty)
             tf.summary.scalar("MeanOfGeneratedImages",genDiscMean)
             tf.summary.scalar("MeanOfTargetImages",targetDiscMean)
@@ -148,9 +152,9 @@ class CLSWGAN:
                 tf.summary.histogram(v.name,v)
                 tf.summary.histogram(v.name+str('grad'),g)
 
-            merged_all = tf.summary.merge_all()
+            self.merged_all = tf.summary.merge_all()
 
-    def train(self, iterBatch, ntrain, _lambda1=10, _cls_weight=1,_lr=0.0001,_beta1=0.5,classifier_checkpoint=49,logdir='./logs/',classifier_modeldir='./models_classifier/',modeldir='./models/'):
+    def train(self, iterBatch, ntrain, _lambda1=10, _cls_weight=1,_lr=0.0001,_beta1=0.5,classifier_checkpoint=49,logdir='./logs/',classifier_modeldir='./src/zeroshot_networks/gan_net/models_classifier/',modeldir='./models/'):
         """
         Args:
             iterBatch: итератор отдающий батчи с запрошенным размером пакета
@@ -179,43 +183,41 @@ class CLSWGAN:
             for var in params:
                 print (var.name+"\t")
 
-            string = classifier_modeldir+'/models_'+str(classifier_checkpoint)+'.ckpt'
+            string = classifier_modeldir+'models_'+str(classifier_checkpoint)+'.ckpt'
             print (string) 
-            try:
+            """try:
                 saver.restore(sess, string)
             except:
                 print("Previous weights not found of classifier") 
                 sys.exit(0)
-
+            """
             print ("Model loaded")
             saver = tf.train.Saver()
             for epoch in range(self.epochCount):
                 for i in range(0, ntrain, self.batch_size):
                     for j in range(self.critic_iter):
-                        batch_feature,batch_att,batch_label,z_rand = next_feed_dict(iterBatch,self.batch_size)
-                        _,discLoss,merged = sess.run([discTrain,discriminatorLoss,merged_all],\
-                                                    feed_dict={ inputImgEmbd:batch_feature,
-                                                                inputTxtEmbd:batch_att,
-                                                                inputNoiseImg:z_rand,
-                                                                inputLabel:batch_label,
-                                                                lambda1:_lambda1,
-                                                                cls_weight:_cls_weight,
-                                                                lr:_lr,
-                                                                beta1:_beta1})
+                        batch_feature,batch_att,batch_label,z_rand = self.next_feed_dict(iterBatch,self.batch_size)
+                        _,discLoss,merged = sess.run([self.discTrain,self.discriminatorLoss,self.merged_all],
+                                                        feed_dict={ self.inputImgEmbd:batch_feature,
+                                                                    self.inputTxtEmbd:batch_att,
+                                                                    self.inputNoiseImg:z_rand,
+                                                                    self.inputLabel:batch_label,
+                                                                    self.lambda1:_lambda1,
+                                                                    self.cls_weight:_cls_weight,
+                                                                    self.lr:_lr})
                         print ("Discriminator loss is:"+str(discLoss))
                         if j == 0:
                             summary_writer.add_summary(merged,k)
 
-                    batch_feature,batch_att,batch_label,z_rand = next_feed_dict(iterBatch,self.batch_size)
-                    _,genLoss,merged = sess.run([generatorTrain,generatorLoss,merged_all],\
-                                            feed_dict={ inputImgEmbd:batch_feature,
-                                                                inputTxtEmbd:batch_att,
-                                                                inputNoiseImg:z_rand,
-                                                                inputLabel:batch_label,
-                                                                lambda1:_lambda1,
-                                                                cls_weight:_cls_weight,
-                                                                lr:_lr,
-                                                                beta1:_beta1})                
+                    batch_feature,batch_att,batch_label,z_rand = self.next_feed_dict(iterBatch,self.batch_size)
+                    _,genLoss,merged = sess.run([self.generatorTrain,self.generatorLoss,self.merged_all],\
+                                                    feed_dict={ self.inputImgEmbd:batch_feature,
+                                                                self.inputTxtEmbd:batch_att,
+                                                                self.inputNoiseImg:z_rand,
+                                                                self.inputLabel:batch_label,
+                                                                self.lambda1:_lambda1,
+                                                                self.cls_weight:_cls_weight,
+                                                                self.lr:_lr})                
                     print ("Generator loss is:"+str(genLoss))
                     summary_writer.add_summary(merged, k)
                     k=k+1
@@ -226,10 +228,17 @@ class CLSWGAN:
   
     def next_feed_dict(self, iterBatch , batch_size):
         batch_feature, batch_labels, batch_att = iterBatch.next_batch(batch_size)
-        batch_label = util.map_label(batch_labels, self.seenclasses)
+        batch_label = self.map_label(batch_labels, self.seenclasses)
         z_rand = np.random.normal(0, 1, [batch_size, self.nz]).astype(np.float32)
         
         return batch_feature,batch_att,batch_label,z_rand
+
+    def map_label(self,label, classes):
+        mapped_label =  np.empty_like(label)
+        for i in range(classes.shape[0]):
+            mapped_label[label==classes[i]] = i    
+
+        return mapped_label
 
     def defoltGenerator(self,x,ngh,resSize,name="generator",reuse=False,isTrainable=True):
 
