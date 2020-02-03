c6b37a94b786a8eb1b9e747d1177eb13231961ca
Krisantis <Krisantis@mail.ru>
2020-02-03 01:59:37 -0800

    generate fitch ADD

--- src/zeroshot_networks/gan_net/clswgan.py
+++ src/zeroshot_networks/gan_net/clswgan.py
@@ -8,7 +8,7 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 os.environ["TF_CPP_MIN_LOG_LEVEL"]='3'
 
 class CLSWGAN:
-    def __init__(self, _seenclasses, _epochCount=2000, _critic_iter=5):
+    def __init__(self, _seenclasses, _epochCount=1, _critic_iter=1):
         """
             _epochCount (nepoch): number of epochs to train for
             _critic_iter: critic iteration, following WGAN-GP
@@ -19,8 +19,7 @@ class CLSWGAN:
         self.seenclasses = _seenclasses
 
         self.g1 = tf.Graph()
-
-
+        self.g2 = tf.Graph()
 
     def initDefoltModel(self,batch_size=64,resSize=2048,attSize=312,nz=312,_beta1=0.5):
         """
@@ -35,8 +34,6 @@ class CLSWGAN:
         self.attSize = attSize
         self.nz = nz
         
-        hiddenLayerGenSize = 4096
-        hiddenLayerDescSize = 1024
         
         with self.g1.as_default():
 
@@ -57,8 +54,6 @@ class CLSWGAN:
             inputGenEmb = tf.concat([self.inputNoiseImg, self.inputTxtEmbd], axis=1)
             
             gen_res = self.defoltGenerator( x=inputGenEmb,
-                                            ngh=hiddenLayerGenSize,
-                                            resSize=resSize,
                                             isTrainable=train,
                                             reuse=reuse)
 
@@ -69,12 +64,10 @@ class CLSWGAN:
             targetEmbd = tf.concat([self.inputImgEmbd,self.inputTxtEmbd], axis=1)
 
             targetDisc = self.defoltDiscriminator(  x=targetEmbd,
-                                                    ndh=hiddenLayerDescSize,
                                                     isTrainable=train,
                                                     reuse=reuse)
             genTargetEmbd = tf.concat([gen_res,self.inputTxtEmbd], axis=1)
             genTargetDisc = self.defoltDiscriminator(   x=genTargetEmbd,
-                                                        ndh=hiddenLayerDescSize,
                                                         isTrainable=train,
                                                         reuse=True)
             
@@ -96,7 +89,6 @@ class CLSWGAN:
             interpolate = tf.concat([interpolates, self.inputTxtEmbd], axis=1)
 
             gradients = tf.gradients(   self.defoltDiscriminator(   x=interpolate,
-                                                                    ndh=hiddenLayerDescSize,
                                                                     reuse=True,
                                                                     isTrainable=train),
                                         [interpolates])[0]
@@ -154,7 +146,7 @@ class CLSWGAN:
 
             self.merged_all = tf.summary.merge_all()
 
-    def train(self, iterBatch, ntrain, _lambda1=10, _cls_weight=1,_lr=0.0001,_beta1=0.5,classifier_checkpoint=49,logdir='./logs/',classifier_modeldir='./src/zeroshot_networks/gan_net/models_classifier/',modeldir='./models/'):
+    def train(self, iterBatch, ntrain, _lambda1=10, _cls_weight=1,_lr=0.0001,_beta1=0.5,classifier_checkpoint=49,logdir='./src/zeroshot_networks/gan_net/logs/',classifier_modeldir='./src/zeroshot_networks/gan_net/models_classifier/',modeldir='./src/zeroshot_networks/gan_net/models/'):
         """
         Args:
             iterBatch: итератор отдающий батчи с запрошенным размером пакета
@@ -168,6 +160,7 @@ class CLSWGAN:
             classifier_modeldir: folder to get classifier model checkpoints
             modeldir: folder to output  model checkpoints
         """
+        self.modeldir = modeldir
         k=1
 
         with tf.Session(graph = self.g1) as sess:
@@ -223,9 +216,83 @@ class CLSWGAN:
                     k=k+1
 
                 
-                saver.save(sess, os.path.join(modeldir, 'models_'+str(epoch)+'.ckpt')) 
+                saver.save(sess, os.path.join(self.modeldir, 'models_'+str(epoch)+'.ckpt')) 
                 print ("Model saved")  
   
+    def initDefoltGenerateModel (self,syn_num=100):
+        """
+        syn_num: number features to generate per class
+        nz: size of the latent z vector
+        """
+        self.syn_num = syn_num
+
+        with self.g2.as_default():
+            # placeholderS :data.unseenclasses, data.attribute,
+            self.syn_att = tf.placeholder(tf.float32,[self.syn_num, self.attSize],name='input_attributes')
+            self.noise_z1 = tf.placeholder(tf.float32,[self.syn_num, self.nz],name='noise')
+            
+            # model definition
+            noise1 = tf.concat([self.noise_z1, self.syn_att], axis=1)
+            self.genResFeatures = self.defoltGenerator( noise1,
+                                            isTrainable=False,
+                                            reuse=False)
+
+    def generateFeature (self,attribute,generateDir = 'src/zeroshot_networks/gan_net/result/'):
+        """
+            attribute: txt attribute matrix for all classes [txtEmb.length() * nclass]
+        """
+
+        syn_res = np.empty((0,self.resSize),np.float32)
+        syn_label = np.empty((0),np.float32)
+
+        with tf.Session(graph = self.g2) as sess:
+            
+            sess.run(tf.global_variables_initializer())
+
+            saver = tf.train.Saver()
+            params = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='generator')
+            
+            saver = tf.train.Saver(var_list=params)
+                    
+            for var in params:
+                print (var.name+"\t")
+            self.modeldir='./src/zeroshot_networks/gan_net/models/'
+            string = self.modeldir+'models_'+str(self.epochCount-1)+'.ckpt'
+            #print (string) 
+            
+            try:
+                saver.restore(sess, string)
+            except:
+                print("Previous weights not found of generator") 
+                sys.exit(0)
+
+            print ("Model loaded")
+
+            saver = tf.train.Saver()
+
+            for i in range(0,self.seenclasses.shape[0]):
+                iclass = self.seenclasses[i]
+                iclass_att = np.reshape(attribute[iclass],(1,self.attSize))
+                #print (iclass_att.shape)
+                #print (iclass_att)
+                batch_att = np.repeat(iclass_att,[self.syn_num],axis=0)
+                #print (batch_att.shape)
+                z_rand = np.random.normal(0, 1, [self.syn_num, self.nz]).astype(np.float32)
+
+                syn_features = sess.run(    self.genResFeatures,
+                                            feed_dict={ self.syn_att:batch_att, 
+                                                        self.noise_z1:z_rand})
+                
+                syn_res = np.vstack((syn_res,syn_features))
+                temp=np.repeat(iclass,[self.syn_num],axis=0)
+                #print (temp.shape)
+                syn_label = np.concatenate((syn_label,temp))
+
+            #print (syn_res.shape)
+            #print (syn_label.shape)
+            np.savetxt(generateDir+'syn_res.txt',syn_res,delimiter=',')
+            np.savetxt(generateDir+'syn_label.txt',syn_label,delimiter=',')
+
     def next_feed_dict(self, iterBatch , batch_size):
         batch_feature, batch_labels, batch_att = iterBatch.next_batch(batch_size)
         batch_label = self.map_label(batch_labels, self.seenclasses)
@@ -240,13 +307,15 @@ class CLSWGAN:
 
         return mapped_label
 
-    def defoltGenerator(self,x,ngh,resSize,name="generator",reuse=False,isTrainable=True):
+    def defoltGenerator(self,x,name="generator",reuse=False,isTrainable=True):
+
+        hiddenLayerGenSize = 4096
 
         with tf.variable_scope(name) as scope:
             if reuse:
                 scope.reuse_variables()
             
-            net = tf.layers.dense(  inputs=x, units=ngh,
+            net = tf.layers.dense(  inputs=x, units=hiddenLayerGenSize,
                                     kernel_initializer=tf.random_normal_initializer(mean=0,stddev=0.02),  \
                                     activation=tf.nn.leaky_relu,
                                     name='gen_fc1',
@@ -254,7 +323,7 @@ class CLSWGAN:
                                     reuse=reuse)        
             
             net = tf.layers.dense(  inputs = net,
-                                    units =resSize,
+                                    units =self.resSize,
                                     kernel_initializer=tf.random_normal_initializer(mean=0,stddev=0.02), \
                                     activation = tf.nn.relu,
                                     name='gen_fc2',
@@ -262,16 +331,17 @@ class CLSWGAN:
                                     reuse=reuse)
             # the output is relu'd as the encoded representation is also the activations by relu
                         
-            return tf.reshape(net, [-1, resSize])
+            return tf.reshape(net, [-1, self.resSize])
 
-    def defoltDiscriminator(self,x,ndh,name="discriminator",reuse=False,isTrainable=True):
-            
+    def defoltDiscriminator(self,x,name="discriminator",reuse=False,isTrainable=True):
+        
+        hiddenLayerDescSize = 1024   
         with tf.variable_scope(name) as scope:
             if reuse:
                 scope.reuse_variables()
             
             net = tf.layers.dense(  inputs=x, 
-                                    units=ndh,
+                                    units=hiddenLayerDescSize,
                                     kernel_initializer=tf.random_normal_initializer(mean=0,stddev=0.02),  \
                                     activation=tf.nn.leaky_relu,
                                     name='disc_fc1',
