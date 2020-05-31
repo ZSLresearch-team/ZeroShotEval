fix for da loss calculation, fix some methods descrriptions

src/zeroshot_networks/cada_vae/cada_vae_model.py
@@ -52,16 +52,16 @@ class VAEModel(nn.Module):
             z_mu: dictionary mapping modalities names to mean layer out.
             z_logvar: dictionary mapping modalities names to variance layer out.
             x_recon: dictionary mapping modalities names to decoder out.
-            z_noize: dictionary mapping modalities names to latent space representation.
+            z_sample: dictionary mapping modalities names to latent space representation sample.
         """
         z_mu = {}
         z_logvar = {}
         x_recon = {}
-        z_noize = {}
+        z_sample = {}
 
         for modality in self.modalities:
-            z_mu[modality], z_logvar[modality], z_noize[modality] = self.encoder[modality](x[modality])
+            z_mu[modality], z_logvar[modality], z_sample[modality] = self.encoder[modality](x[modality])
 
-            x_recon[modality] = self.decoder[modality](z_noize[modality])
+            x_recon[modality] = self.decoder[modality](z_sample[modality])
 
-        return x_recon, z_mu, z_logvar, z_noize
+        return x_recon, z_mu, z_logvar, z_sample

src/zeroshot_networks/cada_vae/cada_vae_train.py
@@ -1,6 +1,6 @@
 import itertools
-from tqdm import tqdm, trange
 import numpy as np
+from tqdm import tqdm, trange
 
 import torch
 import torch.nn as nn
@@ -25,19 +25,18 @@ def train_VAE(config, model, train_loader, optimizer, use_ca_loss=True, use_da_l
     Returns:
         loss_history(list): loss history for 
     """
-    model.to(config.device)
-
     if config.verbose > 1:
         print('\nTrain CADA-VAE model')
 
     tqdm_epoch = trange(config.nepoch, desc='Loss: None. Epoch', unit='epoch', disable=(verbose<=0), leave=True)
 
     loss_history = []
+    model.train()
+
     for epoch in tqdm_epoch:
 
-        model.train()
-
         loss_accum = 0
+
         beta, cross_reconstruction_factor, distance_factor = loss_factors(epoch, config.specific_parameters.warmup)
         tqdm_train_loader = tqdm(train_loader, desc='Loss: None. Batch', unit='batch', disable=(verbose<=1), leave=False)
 
@@ -45,7 +44,6 @@ def train_VAE(config, model, train_loader, optimizer, use_ca_loss=True, use_da_l
 
             for modality, modality_tensor in x.items():
                 x[modality] = modality_tensor.to(config.device).float()
-                x[modality].requires_grad = False
 
             x_recon, z_mu, z_logvar, z_noize = model(x)
 
@@ -56,7 +54,7 @@ def train_VAE(config, model, train_loader, optimizer, use_ca_loss=True, use_da_l
 
             if use_ca_loss:
                 loss += loss_ca * cross_reconstruction_factor
-            if use_da_loss:
+            if use_da_loss and (distance_factor > 0):
                 loss += loss_da * distance_factor
 
             optimizer.zero_grad()
@@ -93,7 +91,9 @@ def eval_VAE(model, test_loader, test_modality, reparametrize_with_noise=True):
     with torch.no_grad():
         zsl_emb = torch.Tensor().to('cpu')
         labels = torch.Tensor().long().to('cpu')
+
         for _i_step, (x, _y) in enumerate(test_loader):
+            # !
             if test_modality == 'img':
                 x = x[test_modality].float().to('cuda:0')
             else:
@@ -136,17 +136,16 @@ def compute_cada_losses(decoder, x, x_recon, z_mu, z_logvar, z_noize, beta, *arg
     for modality in z_mu.keys():
         # Calculate reconstruction and kld loss for each modality
         loss_recon += reconstruction_loss(x[modality], x_recon[modality], **kwargs)
-
         loss_kld += 0.5 * (1 + z_logvar[modality] - z_mu[modality].pow(2) - z_logvar[modality].exp()).sum(dim=1).mean()
 
+    # Calculate standart vae loss as sum of reconstion loss and Kullback–Leibler divergence
     loss_vae = loss_recon - beta * loss_kld
 
     for (modality_1, modality_2) in itertools.combinations(z_mu.keys(), 2):
         # Calulate cross allignment and distribution allignment loss for each pair of modalities
-        loss_da += compute_da_loss(z_mu[modality_1], z_mu[modality_2], z_logvar[modality_1], z_logvar[modality_2])
+        loss_da += compute_da_loss(z_mu[modality_1], z_logvar[modality_1], z_mu[modality_2], z_logvar[modality_2])
         loss_ca += compute_ca_loss(decoder[modality_1], decoder[modality_2], x[modality_1], x[modality_2],
                                    z_noize[modality_1], z_noize[modality_2], *args, **kwargs)
-
 
     return loss_vae, loss_ca, loss_da
 
@@ -172,7 +171,7 @@ def compute_da_loss(z_mu_1, z_logvar_1, z_mu_2, z_logvar_2):
 
     return loss_da
 
-def compute_ca_loss(decoder_1, decoder_2, x_1, x_2, z_noize_1, z_noize_2, *args, **kwargs):
+def compute_ca_loss(decoder_1, decoder_2, x_1, x_2, z_sample_1, z_sample_2, *args, **kwargs):
     r"""
     Computes cross alignment loss.
     First modality original input compares to reconstrustion wich uses first modality decoder 
@@ -183,8 +182,8 @@ def compute_ca_loss(decoder_1, decoder_2, x_1, x_2, z_noize_1, z_noize_2, *args,
         decoder_2(nn.module): decoder for second modality.
         x_1(Tensor): first modality original input.
         x_2(Tensor): second modality original input.
-        z_noize_1(Tensor): first modality noize encoder out.
-        z_noize_2(Tensor): first modality noize encoder out.
+        z_sample_1(Tensor): first modality latent representation sample.
+        z_sample_2(Tensor): second modality latent representation sample.
 
     Returns:
         loss_ca: cross alignment loss over two given modalities.
@@ -192,15 +191,15 @@ def compute_ca_loss(decoder_1, decoder_2, x_1, x_2, z_noize_1, z_noize_2, *args,
     decoder_1.eval()
     decoder_2.eval()
 
-    x_recon_1 = decoder_1(z_noize_2)
-    x_recon_2 = decoder_2(z_noize_1)
+    x_recon_1 = decoder_1(z_sample_2)
+    x_recon_2 = decoder_2(z_sample_1)
 
     loss_ca = reconstruction_loss(x_1, x_recon_1, *args, **kwargs) + \
                 reconstruction_loss(x_2, x_recon_2, *args, **kwargs)
 
     return loss_ca
 
-def reconstruction_loss(x, x_recon, recon_loss_norm="l2", **kwargs):
+def reconstruction_loss(x, x_recon, recon_loss_norm="l1", **kwargs):
     r"""
     Computes reconstruction loss.
 
@@ -231,6 +230,7 @@ def loss_factors(current_epoch, warmup):
     Returns:
         beta, cross_reconstruction_factor, distance_factor
     """
+    # Beta factor
     if current_epoch < warmup.beta.start_epoch:
         beta = 0
     elif current_epoch >= warmup.beta.end_epoch:
@@ -239,6 +239,7 @@ def loss_factors(current_epoch, warmup):
         beta = 1.0 * (current_epoch - warmup.beta.start_epoch) / \
             (warmup.beta.end_epoch - warmup.beta.start_epoch) * warmup.beta.factor
 
+    # Cross-reconstruction factor
     if current_epoch < warmup.cross_reconstruction.start_epoch:
         cross_reconstruction_factor = 0
     elif current_epoch >= warmup.cross_reconstruction.end_epoch:
@@ -248,6 +249,7 @@ def loss_factors(current_epoch, warmup):
             warmup.cross_reconstruction.end_epoch - warmup.cross_reconstruction.start_epoch) * \
                 warmup.cross_reconstruction.factor
 
+    # Distribution alignment factor
     if current_epoch < warmup.distance.start_epoch:
         distance_factor = 0
     elif current_epoch >= warmup.distance.end_epoch:
@@ -259,24 +261,23 @@ def loss_factors(current_epoch, warmup):
     return beta, cross_reconstruction_factor, distance_factor
 
 def generate_synthetic_dataset(model_config, dataset, model):
-    """
+    r"""
     Generates synthetic dataset via trained zsl model to cls training
 
     Args:
         model_config(dict): dictionary with setting for model.
-        datset: original dataset.
+        dataset: original dataset.
         model: pretrained CADA-VAE model.
 
     Returns:
         zsl_emb_dataset: sythetic dataset for classifier .
         csl_train_indice: train indicies.
         csl_test_indice: test indicies.
-
     """
     zsl_emb_object_indice, zsl_emb_class, zsl_emb_class_label = \
         dataset.get_zsl_emb_indice(model_config.specific_parameters.samples_per_modality_class)
 
-        # Set CADA-Vae model to evaluate mode
+    # Set CADA-Vae model to evaluate mode
     model.eval()
 
     # Generate zsl embeddings for train seen images
@@ -348,7 +349,7 @@ def remap_labels(labels, classes):
 
 def VAE_train_procedure(model_config, dataset, gen_syn_data=True, save_model=False, save_dir='../../../model/'):
     """
-    Starts Cada-vae model training and generates zsl_embedding dataset for classifier training.
+    Starts CADA-VAE model training and generates zsl_embedding dataset for classifier training.
 
     Args:
         model_config(dict): dictionary with setting for model.
@@ -370,6 +371,8 @@ def VAE_train_procedure(model_config, dataset, gen_syn_data=True, save_model=Fal
                      use_dropout=model_config.specific_parameters.use_dropout
                      )
 
+    if model_config.verbose > 2:
+        print(model)
     model.to(model_config.device)
     # Model training
     optimizer = torch.optim.Adam(model.parameters(), lr=model_config.specific_parameters.lr_gen_model,

src/zeroshot_networks/cada_vae/vae_networks.py
@@ -43,11 +43,11 @@ class EncoderTemplate(nn.Module):
         modules = []
 
         for i in range(len(self.layer_sizes)-2):
-            if use_dropout:
-                modules.append(nn.Dropout(p=0.2))
             modules.append(nn.Linear(self.layer_sizes[i], self.layer_sizes[i+1]))
             if use_bn:
                 modules.append(nn.BatchNorm1d(self.layer_sizes[i+1]))
+            if use_dropout:
+                modules.append(nn.Dropout(p=0.2))
             modules.append(nn.ReLU())
 
         self.feature_encoder = nn.Sequential(*modules)
@@ -62,7 +62,7 @@ class EncoderTemplate(nn.Module):
         Returns:
             z_mu: mean layer out.
             z_logvar: variance layer out.
-            z_noize: latent space representation.
+            z_sample: sample from latent space representation.
         """
         hidden = self.feature_encoder(x)
 
@@ -73,9 +73,9 @@ class EncoderTemplate(nn.Module):
         eps = torch.randn(z_logvar.size()[0], 1).to(z_logvar.device)
         eps = eps.expand(z_logvar.size())
 
-        z_noize = eps * std + z_mu
+        z_sample = eps * std + z_mu
 
-        return z_mu, z_logvar, z_noize
+        return z_mu, z_logvar, z_sample
 
 
 class DecoderTemplate(nn.Module):